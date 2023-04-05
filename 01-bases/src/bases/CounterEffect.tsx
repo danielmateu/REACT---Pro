2559ddfbc7b1140872ebd951d55f8f3f728ee01b
Daniel Mateu <danielmateu86@gmail.com>
2023-04-05 12:00:45 +0200

    useRef + timeLines

--- 01-bases/src/bases/CounterEffect.tsx
+++ 01-bases/src/bases/CounterEffect.tsx
@@ -1,5 +1,5 @@
 import { gsap } from "gsap";
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 
 
 const MAXIMUN_COUNT = 10;
@@ -7,17 +7,7 @@ const MAXIMUN_COUNT = 10;
 export const CounterEffect = () => {
 
     const [counter, setCounter] = useState(5);
-
-    useEffect(() => {
-        // console.log('UseEffect');
-        if(counter < 10) return
-        
-        console.log('%cSe llegó a 10', 'color: green; background-color: black; font-size: 20px; font-weight: bold;');
-        gsap.to('h2', { duration: 1, color: 'red', y: -10,ease: 'ease.out'  }).then(() => {
-            gsap.to('h2', { duration: 1, y: 0, ease: 'bounce.out' });
-        });
-    }, [counter])
-    
+    const counterElement = useRef<HTMLHeadingElement>(null);
 
     const handleCLick = () => {
         // if MAXIMUN_COUNT => console.log('Se llegó a 10') and stop
@@ -25,10 +15,25 @@ export const CounterEffect = () => {
         setCounter(prev => Math.min(prev + 1, MAXIMUN_COUNT));
     }
 
+    useLayoutEffect(() => {
+        // console.log('UseEffect');
+        if (counter < 10) return
+
+        console.log('%cSe llegó a 10', 'color: green; background-color: black; font-size: 20px; font-weight: bold;');
+
+        const timeline = gsap.timeline({});
+
+        timeline
+            .to(counterElement.current, { duration: 1, color: 'red', y: -10, ease: 'ease.out' })
+            .to(counterElement.current, { duration: 1, color: 'red', y: 0, ease: 'bounce.out' }).repeat(1);
+    }, [counter])
+
+
+
     return (
         <>
             <h1>Counter Effect:</h1>
-            <h2>{counter}</h2>
+            <h2 ref={counterElement}>{counter}</h2>
 
             <button onClick={handleCLick}>+1</button>
         </>
